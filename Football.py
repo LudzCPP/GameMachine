334ebcbb26220d278315cda97b159239ed3b1e02
LudzCPP <kacper.janowicz@onet.pl>
2021-08-24 11:03:16 +0200

    Add ball movement

--- Football.py
+++ Football.py
@@ -2,6 +2,8 @@ import pygame
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 900, 500
 PLAYER_WIDTH, PLAYER_HEIGHT = 20, 100
+BALL_WIDTH, BALL_HEIGHT = 15, 15
+BALL_SPEED = 5
 PLAYER_SPEED = 5
 
 WHITE = (255, 255, 255)
@@ -19,6 +21,9 @@ class Football:
 	def main(self):
 		left_player = pygame.Rect(50, WINDOW_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
 		right_player = pygame.Rect(WINDOW_WIDTH - 50 - PLAYER_WIDTH, WINDOW_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
+		ball = pygame.Rect(WINDOW_WIDTH / 2 - BALL_WIDTH / 2, WINDOW_HEIGHT / 2 - BALL_HEIGHT / 2, BALL_WIDTH, BALL_HEIGHT)
+
+		ball_direction = [1, -1]
 
 		clock = pygame.time.Clock()
 
@@ -31,12 +36,15 @@ class Football:
 					pygame.display.quit()
 
 			self.player_movement_handle(left_player, right_player)
-			self.draw_window(left_player, right_player)
+			ball_direction = self.ball_movement_handle(ball, ball_direction, left_player, right_player)
+			self.draw_window(left_player, right_player, ball)
 
-	def draw_window(self, left, right):
+	def draw_window(self, left, right, ball):
 		self.window.fill(BLACK)
 		pygame.draw.rect(self.window, WHITE, left)
 		pygame.draw.rect(self.window, WHITE, right)
+		pygame.draw.rect(self.window, WHITE, ball)
+
 		pygame.display.update()
 
 	def player_movement_handle(self, left, right):
@@ -50,3 +58,16 @@ class Football:
 			right.y -= PLAYER_SPEED
 		if pressed_keys[pygame.K_DOWN] and right.y < WINDOW_HEIGHT - PLAYER_HEIGHT:
 			right.y += PLAYER_SPEED
+
+	def ball_movement_handle(self, ball, direction, left, right):
+		ball.x += BALL_SPEED * direction[0]
+		ball.y += BALL_SPEED * direction[1]
+
+		if ball.y <= 0:
+			direction[1] *= -1
+		if ball.y >= WINDOW_HEIGHT - BALL_HEIGHT:
+			direction[1] *= -1
+		if ball.colliderect(left) or ball.colliderect(right):
+			direction[0] *= -1
+
+		return direction
