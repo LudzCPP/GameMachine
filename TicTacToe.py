9e31ff9c4bb248bfd1c724826957bb4577fd17ba
LudzCPP <kacper.janowicz@onet.pl>
2021-09-19 18:04:34 +0200

    Finish mechanics of TicTacToe

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,6 +9,14 @@ BORDER = [
 	pygame.Rect(0, 2 * (WINDOW_HEIGHT / 3 - BORDER_WIDTH / 2), WINDOW_WIDTH, BORDER_WIDTH)
 ]
 
+WINNING_COMBINATIONS = [
+	[[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], [[2, 0], [2, 1], [2, 2]],
+	[[0, 0], [1, 0], [2, 0]], [[0, 1], [1, 1], [2, 1]], [[0, 2], [1, 2], [2, 2]],
+	[[0, 0], [1, 1], [2, 2]], [[0, 2], [1, 1], [2, 0]]
+]
+
+GAME_END = pygame.USEREVENT + 1
+
 SIGNS = {
 	True: 'X',
 	False: 'O'
@@ -18,21 +26,26 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FPS = 60
 
+board = []
+
 
 class TicTacToe:
 	def __init__(self):
 		self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 		self.current_sign = True
-		self.board = [
-			['-', '-', '-'],
-			['-', '-', '-'],
-			['-', '-', '-']
-		]
 		pygame.display.set_caption("Tic Tac Toe")
 		self.main()
 
 	def main(self):
 		clock = pygame.time.Clock()
+
+		global board
+
+		board = [
+			['-', '-', '-'],
+			['-', '-', '-'],
+			['-', '-', '-']
+		]
 
 		run = True
 		while run:
@@ -43,7 +56,14 @@ class TicTacToe:
 					pygame.display.quit()
 				if event.type == pygame.MOUSEBUTTONUP:
 					pos = pygame.mouse.get_pos()
-					self.click_handle(pos, self.current_sign)
+					self.click_handle(pos)
+				if event.type == GAME_END:
+					pygame.time.delay(3000)
+					board = [
+						['-', '-', '-'],
+						['-', '-', '-'],
+						['-', '-', '-']
+					]
 
 			self.draw_window()
 
@@ -56,23 +76,25 @@ class TicTacToe:
 
 		pygame.display.update()
 
-	def click_handle(self, pos, current_sign):
+	def click_handle(self, pos):
+		global board
+
 		col = int(pos[0] // (WINDOW_WIDTH / 3 - BORDER_WIDTH / 2))
 		row = int(pos[1] // (WINDOW_HEIGHT / 3 - BORDER_WIDTH / 2))
 
-		if self.board[row][col] == '-':
-			self.board[row][col] = SIGNS[current_sign]
-			self.current_sign = not current_sign
+		if board[row][col] == '-':
+			board[row][col] = SIGNS[self.current_sign]
+			self.current_sign = not self.current_sign
 
-		print(BORDER[0].x)
-
-		for row in self.board:
-			print(row)
+		if self.check_win():
+			self.win_handle(self.check_win())
 
 	def draw_shapes(self):
+		global board
+
 		for row in range(3):
 			for col in range(3):
-				if self.board[row][col] == 'O':
+				if board[row][col] == 'O':
 					pygame.draw.circle(
 						self.window,
 						WHITE,
@@ -80,7 +102,7 @@ class TicTacToe:
 						50,
 						10
 					)
-				elif self.board[row][col] == 'X':
+				elif board[row][col] == 'X':
 					self.draw_x(row, col)
 
 	def draw_x(self, row, col):
@@ -99,3 +121,26 @@ class TicTacToe:
 			(col * 200 + 130, row * 200 + 140),
 			15
 		)
+
+	def check_win(self):
+		global board
+
+		for comb in WINNING_COMBINATIONS:
+			if board[comb[0][0]][comb[0][1]] == board[comb[1][0]][comb[1][1]] == board[comb[2][0]][comb[2][1]] != '-':
+				return comb
+		return False
+
+	def win_handle(self, comb):
+		global board
+
+		for row in range(3):
+			for col in range(3):
+				if [row, col] not in comb:
+					board[row][col] = '-'
+
+		pygame.display.update()
+
+		pygame.event.post(pygame.event.Event(GAME_END))
+
+
+
